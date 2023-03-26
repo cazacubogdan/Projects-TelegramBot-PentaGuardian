99b3edbd227127471e68d8bd2390b0b35cf21b2f
Bogdan Cazacu <contact@cazacubogdan.com>
2023-03-26 23:42:50 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import logging
 import re
 import langdetect
 from telegram import Update
-from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from telegram.ext import Updater, CommandHandler, MessageHandler, filters, CallbackContext
 from telegram.error import BadRequest
 
 def read_api_key(filename):

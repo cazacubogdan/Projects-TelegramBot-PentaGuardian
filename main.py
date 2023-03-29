09b5b455248841afde12c3f0bffa53ae1b4a38ae
Bogdan Cazacu <contact@cazacubogdan.com>
2023-03-29 10:55:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -109,8 +109,8 @@ def main():
     dp.add_handler(MessageHandler(Filters.text & ~Filters.command, check_language))
     dp.add_error_handler(error)
 
-updater.start_polling()
-updater.idle()
+    updater.start_polling()
+    updater.idle()
 
 if name == 'main':
     main()
